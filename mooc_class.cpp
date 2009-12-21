faaf82e37a51dd7eccd51373d1aad0ef40ff0e08
neswold <>
2009-12-21 16:29:07 +0000

    Install autoload functions.

--- mooc_class.cpp
+++ mooc_class.cpp
@@ -1,1 +1,20 @@
 // $Id$
+
+#include <cstdio>
+#include <mooc++-4.0.h>
+
+// Local prototypes...
+
+static void register_class(void) __attribute__((constructor));
+static void term(void) __attribute__((destructor));
+
+static void register_class(void)
+{
+    printf("V473 module loaded.\n");
+}
+
+static void term(void)
+{
+    printf("ERROR: the V473 module is not designed to be unloaded! This\n"
+	   "       system is now unstable -- a reboot is recommended!");
+}
