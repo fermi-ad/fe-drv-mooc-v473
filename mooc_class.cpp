bc6e6eb6f1c9f902162217740c1dccd68af82f20
neswold <>
2009-12-21 18:29:20 +0000

    Add a function to create an instance of the MOOC class.

--- mooc_class.cpp
+++ mooc_class.cpp
@@ -1,6 +1,7 @@
 // $Id$
 
 #include <cstdio>
+#include <memory>
 #include <mooc++-4.0.h>
 #include "v473.h"
 
@@ -8,10 +9,46 @@
 
 static void term(void) __attribute__((destructor));
 
+// This function gets called when trying to unload the module under
+// VxWorks. Since we don't support it, let the user know she screwed
+// up the machine.
+
 static void term(void)
 {
     printf("ERROR: the V473 module is not designed to be unloaded! This\n"
 	   "       system is now unstable -- a reboot is recommended!");
+}
+
+// This function initializes an instance of the MOOC V473 class.
+
+static STATUS objInit(short const oid, void const*, void const*,
+		      void* ivs)
+{
+    return OK;
+}
+
+STATUS v473_create_mooc_instance(unsigned short const oid,
+				 uint8_t const addr,
+				 uint8_t const intVec)
+{
+    try {
+	short const cls = find_class("V473");
+
+	if (cls == -1)
+	    throw std::runtime_error("V473 class is not registered with MOOC");
+
+	std::auto_ptr<V473::Card> ptr(v473_create(addr, intVec));
+
+	if (create_instance(oid, cls, ptr.get(), "V473") != NOERR)
+	    throw std::runtime_error("problem creating an instance");
+
+	ptr.release();
+	return OK;
+    }
+    catch (std::exception const& e) {
+	printf("ERROR: %s\n", e.what());
+	return ERROR;
+    }
 }
 
 // Creates the MOOC class for the V473. Instances of V473::Card
