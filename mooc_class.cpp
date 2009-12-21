d352ed3de959d3bbdb07506d592584232d06eb5a
neswold <>
2009-12-21 16:55:08 +0000

    Create function that defines the V473 MOOC class.

--- mooc_class.cpp
+++ mooc_class.cpp
@@ -2,19 +2,34 @@
 
 #include <cstdio>
 #include <mooc++-4.0.h>
+#include "v473.h"
 
 // Local prototypes...
 
-static void register_class(void) __attribute__((constructor));
 static void term(void) __attribute__((destructor));
-
-static void register_class(void)
-{
-    printf("V473 module loaded.\n");
-}
 
 static void term(void)
 {
     printf("ERROR: the V473 module is not designed to be unloaded! This\n"
 	   "       system is now unstable -- a reboot is recommended!");
+}
+
+// Creates the MOOC class for the V473. Instances of V473::Card
+// objects can be attached to instances of this MOOC class.
+
+STATUS v473_create_mooc_class(uint8_t cls)
+{
+    if (cls < 16) {
+	printf("MOOC class codes need to be 16, or greater.\n");
+	return ERROR;
+    }
+
+    if (NOERR != create_class(cls, 0, 0, 1, sizeof(V473::Card*)))
+	return ERROR;
+    if (NOERR != name_class(cls, "V473"))
+	return ERROR;
+    if (NOERR != add_class_msg(cls, Init, (PMETHOD) objInit))
+	return NOERR;
+
+    return OK;
 }
