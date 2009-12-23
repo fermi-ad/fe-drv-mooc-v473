440da7cb862c54191bc3433d8b39a61abbe6c915
neswold <>
2009-12-23 19:39:11 +0000

    Show where the semaphore is.

--- mooc_class.cpp
+++ mooc_class.cpp
@@ -185,7 +185,9 @@ static STATUS devReading(short const cls, RS_REQ const* const req,
 	return OK;
     }
     catch (std::exception const& e) {
-	printf("%s: exception '%s'\n", __func__, e.what());
+	printf("%s: exception '%s'\n"
+	       "(BTW, the mutex is at %p\n", __func__, e.what(),
+	       &(*ivs)->mutex);
 	return ERR_DEVICEERROR;
     }
 }
