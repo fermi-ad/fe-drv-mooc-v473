711847e5f27a4a697115d24b8e1b0f8734807899
neswold <>
2010-04-27 15:18:14 +0000

    Add many properties:
    	All the simple tables are done.
    
    	Can read and write the DACs.
    
    	Still need to set the F(t) tables, the software version
    	device, and several properties for basic control and status.

--- mooc_class.cpp
+++ mooc_class.cpp
@@ -12,16 +12,20 @@ typedef unsigned char type_t;
 #define OMSPDEF_TO_CHAN(a)	((chan_t) (a).chan)
 #define OMSPDEF_TO_TYPE(a)	((type_t) (a).typ)
 
-#define	REQ_TO_CHAN(a)	OMSPDEF_TO_CHAN(*(OMSP_DEF const*) &(a)->OMSP)
-#define	REQ_TO_TYPE(a)	OMSPDEF_TO_TYPE(*(OMSP_DEF const*) &(a)->OMSP)
-#define REQ_TO_SUBCODE(req)   ((((OMSP_DEF const*)&(req)->OMSP)->chan & 0xf0) >> 4)
-#define REQ_TO_453CHAN(req)   (((OMSP_DEF const*)&(req)->OMSP)->chan & 0x3)
+#define	REQ_TO_CHAN(a)		OMSPDEF_TO_CHAN(*(OMSP_DEF const*) &(a)->OMSP)
+#define	REQ_TO_TYPE(a)		OMSPDEF_TO_TYPE(*(OMSP_DEF const*) &(a)->OMSP)
+#define REQ_TO_SUBCODE(req)	((((OMSP_DEF const*)&(req)->OMSP)->chan & 0xf0) >> 4)
+#define REQ_TO_453CHAN(req)	(((OMSP_DEF const*)&(req)->OMSP)->chan & 0x3)
+
 // for Subcode 15 only...
-#define REQ_TO_453TYPE(req)  ((((OMSP_DEF*)&(req)->OMSP)->typ >> 6) & 0x3)
-#define REQ_TO_TABLE(req) (((OMSP_DEF*)&(req)->OMSP)->typ & 0x3f)
-#define REQ_TO_START(req) ((req)->misc2 & 0xff)
-#define REQ_TO_DELTA(req) ((req)->misc2 >> 12)
-#define REQ_TO_TOTAL(req) (((req)->misc2 >> 8) & 0xf)
+
+#define REQ_TO_453TYPE(req)	((((OMSP_DEF*)&(req)->OMSP)->typ >> 6) & 0x3)
+#define REQ_TO_TABLE(req)	(((OMSP_DEF*)&(req)->OMSP)->typ & 0x3f)
+#define REQ_TO_START(req)	((req)->misc2 & 0xff)
+#define REQ_TO_DELTA(req)	((req)->misc2 >> 12)
+#define REQ_TO_TOTAL(req)	(((req)->misc2 >> 8) & 0xf)
+
+#define DATAS(req) (*(unsigned short const*)(req)->data)
 
 // Junk class. Some versions of GCC don't honor the
 // constructor/destructor attributes unless there's a C++ global
@@ -53,15 +57,18 @@ static STATUS objInit(short const oid, V473::Card* const ptr, void const*,
     return OK;
 }
 
-typedef bool (V473::Card::*STableCallback)(vwpp::Lock const&, uint16_t,
-					   uint16_t, uint16_t*, uint16_t);
+typedef bool (V473::Card::*STableReadCallback)(vwpp::Lock const&, uint16_t,
+					       uint16_t, uint16_t*, uint16_t);
+typedef bool (V473::Card::*STableWriteCallback)(vwpp::Lock const&, uint16_t,
+						uint16_t, uint16_t const*,
+						uint16_t);
 
 // Reading the simple tables and the maps are done practically the
 // same way. This function encapsulates the similarities.
 
-static STATUS readSimpleTable(RS_REQ const* req, size_t const entrySize,
+static STATUS readSimpleTable(RS_REQ const* const req, size_t const entrySize,
 			      size_t const maxSize, V473::Card* const obj,
-			      STableCallback mt, uint16_t* const ptr)
+			      STableReadCallback mt, uint16_t* const ptr)
 {
     if (REQ_TO_453CHAN(req) >= 4)
 	return ERR_BADCHN;
@@ -77,18 +84,80 @@ static STATUS readSimpleTable(RS_REQ const* req, size_t const entrySize,
     if (!(obj->*mt)(lock, REQ_TO_453CHAN(req), req->OFFSET / entrySize,
 		    ptr, req->ILEN / entrySize))
 	return ERR_MISBOARD;
-    return OK;
+    return NOERR;
 }
 
-static STATUS devReading(short const cls, RS_REQ const* const req,
-			 void* const rep, V473::Card* const* const ivs)
+static STATUS writeSimpleTable(RS_REQ const* const req, size_t const entrySize,
+			      size_t const maxSize, V473::Card* const obj,
+			      STableWriteCallback mt,
+			       uint16_t const* const ptr)
+{
+    if (REQ_TO_453CHAN(req) >= 4)
+	return ERR_BADCHN;
+    if (req->ILEN % entrySize || req->ILEN > maxSize)
+	return ERR_BADLEN;
+    if (req->OFFSET % entrySize || req->OFFSET > maxSize - entrySize)
+	return ERR_BADOFF;
+    if (req->OFFSET + req->ILEN > maxSize)
+	return ERR_BADOFLEN;
+
+    vwpp::Lock lock(obj->mutex, 100);
+
+    if (!(obj->*mt)(lock, REQ_TO_453CHAN(req), req->OFFSET / entrySize,
+		    ptr, req->ILEN / entrySize))
+	return ERR_MISBOARD;
+    return NOERR;
+}
+
+static STATUS devReading(short, RS_REQ const* const req, void* const rep,
+			 V473::Card* const* const ivs)
 {
     try {
 	switch (REQ_TO_SUBCODE(req)) {
 	 case 1:		// G(i) tables. We dont have these, so fake it.
-	    if (rep)
-		memset(rep, 0, req->ILEN);
-	    break;
+	 case 2:		// F(t) tables.
+	 case 3:		// Delay Table
+	 case 4:		// Offset Table
+	 case 5:
+	 case 6:		// Scale Factor Table
+	 case 9:		// Frequency Table
+	 case 10:		// Phase Table
+	 default:
+	    return ERR_BADPROP;
+	}
+	return NOERR;
+    }
+    catch (std::exception const& e) {
+	printf("%s: exception '%s'\n", __func__, e.what());
+	return ERR_DEVICEERROR;
+    }
+}
+
+static STATUS devReadSetting(short, RS_REQ const* const req,
+			     void* const rep, V473::Card* const* const ivs)
+{
+    try {
+	switch (REQ_TO_SUBCODE(req)) {
+	 case 1:		// G(i) tables. We dont have these, so fake it.
+	     {
+		 static size_t const entrySize = 4;
+		 static size_t const maxSize = 15 * 64 * entrySize;
+		 size_t const length = req->ILEN;
+		 size_t const offset = req->OFFSET;
+
+		 if (REQ_TO_453CHAN(req) >= 4)
+		     return ERR_BADCHN;
+		 if (length % entrySize || length > maxSize)
+		     return ERR_BADLEN;
+		 if (offset % entrySize || offset > maxSize - entrySize)
+		     return ERR_BADOFF;
+		 if (offset + length > maxSize)
+		     return ERR_BADOFLEN;
+
+		 if (rep)
+		     memset(rep, 0, req->ILEN);
+	     }
+	     break;
 
 	 case 2:		// F(t) tables.
 	     {
@@ -129,7 +198,7 @@ static STATUS devReading(short const cls, RS_REQ const* const req,
 
 	 case 5:
 	     {
-		 static STableCallback const mt[] = {
+		 static STableReadCallback const mt[] = {
 		     &V473::Card::getRampMap,
 		     &V473::Card::getScaleFactorMap,
 		     &V473::Card::getOffsetMap,
@@ -168,13 +237,33 @@ static STATUS devReading(short const cls, RS_REQ const* const req,
 			 offset += total;
 			 length -= total;
 		     }
-		 return NOERR;
 	     }
+	     return NOERR;
 
 	 case 6:		// Scale Factor Table
 	    return readSimpleTable(req, 2, 64, *ivs,
 				   &V473::Card::getScaleFactors,
 				   (uint16_t*) rep);
+
+	 case 8:
+	     {
+		 size_t const length = req->ILEN;
+		 size_t const offset = req->OFFSET;
+		 size_t const chan = REQ_TO_453CHAN(req);
+
+		 if (chan >= 4)
+		     return ERR_BADCHN;
+		 if (length > sizeof(uint16_t))
+		     return ERR_BADLEN;
+		 if (offset != 0)
+		     return ERR_BADOFF;
+
+		 vwpp::Lock lock((*ivs)->mutex, 100);
+
+		 return (*ivs)->getDAC(lock, chan, (uint16_t*) rep) ?
+		     NOERR : ERR_MISBOARD;
+	     }
+	     break;
 
 	 case 9:		// Frequency Table
 	    return readSimpleTable(req, 2, 64, *ivs,
@@ -189,7 +278,130 @@ static STATUS devReading(short const cls, RS_REQ const* const req,
 	 default:
 	    return ERR_BADPROP;
 	}
-	return OK;
+	return NOERR;
+    }
+    catch (std::exception const& e) {
+	printf("%s: exception '%s'\n", __func__, e.what());
+	return ERR_DEVICEERROR;
+    }
+}
+
+static STATUS devSetting(short, RS_REQ* req, void*,
+			 V473::Card* const* const obj)
+{
+    try {
+	switch (REQ_TO_SUBCODE(req)) {
+	 case 1:
+	     {
+		 static size_t const entrySize = 4;
+		 static size_t const maxSize = 15 * 64 * entrySize;
+		 size_t const length = req->ILEN;
+		 size_t const offset = req->OFFSET;
+
+		 if (REQ_TO_453CHAN(req) >= 4)
+		     return ERR_BADCHN;
+		 if (length % entrySize || length > maxSize)
+		     return ERR_BADLEN;
+		 if (offset % entrySize || offset > maxSize - entrySize)
+		     return ERR_BADOFF;
+		 if (offset + length > maxSize)
+		     return ERR_BADOFLEN;
+	     }
+	     return NOERR;
+
+	 case 3:		// Delay Table
+	    return writeSimpleTable(req, 2, 64, *obj,
+				    &V473::Card::setDelays,
+				    (uint16_t const*) req->data);
+
+	 case 4:		// Offset Table
+	    return writeSimpleTable(req, 2, 64, *obj,
+				    &V473::Card::setOffsets,
+				    (uint16_t const*) req->data);
+
+	 case 5:
+	     {
+		 static STableWriteCallback const mt[] = {
+		     &V473::Card::setRampMap,
+		     &V473::Card::setScaleFactorMap,
+		     &V473::Card::setOffsetMap,
+		     &V473::Card::setFrequencyMap,
+		     &V473::Card::setPhaseMap
+		 };
+		 static size_t const nTables = sizeof(mt) / sizeof(*mt);
+		 static size_t const entrySize = 2;
+		 static size_t const tableSize = 32 * entrySize;
+		 static size_t const maxSize = nTables * tableSize;
+
+		 size_t length = req->ILEN;
+		 size_t offset = req->OFFSET;
+		 uint16_t const* ptr = (uint16_t const*) req->data;
+
+		 if (REQ_TO_453CHAN(req) >= 4)
+		     return ERR_BADCHN;
+		 if (length % entrySize || length > maxSize)
+		     return ERR_BADLEN;
+		 if (offset % entrySize || offset > maxSize - entrySize)
+		     return ERR_BADOFF;
+		 if (offset + length > maxSize)
+		     return ERR_BADOFLEN;
+
+		 vwpp::Lock lock((*obj)->mutex, 100);
+
+		 for (size_t ii = 0; length > 0 && ii < nTables; ++ii)
+		     if (offset < ii * tableSize) {
+			 size_t const total(std::min(length, (ii * tableSize) - offset));
+
+			 if (!((*obj)->*mt[ii])(lock, REQ_TO_453CHAN(req),
+						(offset % 64) / 2,
+						ptr, total / 2))
+			     return ERR_MISBOARD;
+			 ptr += total / 2;
+			 offset += total;
+			 length -= total;
+		     }
+	     }
+	     return NOERR;
+
+	 case 6:		// Scale Factor Table
+	    return writeSimpleTable(req, 2, 64, *obj,
+				    &V473::Card::setScaleFactors,
+				    (uint16_t const*) req->data);
+
+	 case 8:
+	     {
+		 size_t const length = req->ILEN;
+		 size_t const offset = req->OFFSET;
+		 size_t const chan = REQ_TO_453CHAN(req);
+
+		 if (chan >= 4)
+		     return ERR_BADCHN;
+		 if (length > sizeof(uint16_t))
+		     return ERR_BADLEN;
+		 if (offset != 0)
+		     return ERR_BADOFF;
+
+		 vwpp::Lock lock((*obj)->mutex, 100);
+
+		 return (*obj)->setDAC(lock, chan, DATAS(req)) ?
+		     NOERR : ERR_MISBOARD;
+	     }
+	     break;
+
+	 case 9:		// Frequency Table
+	    return writeSimpleTable(req, 2, 64, *obj,
+				    &V473::Card::setFrequencies,
+				    (uint16_t const*) req->data);
+
+	 case 10:		// Phase Table
+	    return writeSimpleTable(req, 2, 64, *obj,
+				    &V473::Card::setPhases,
+				    (uint16_t const*) req->data);
+
+	 default:
+	    return ERR_BADPROP;
+	}
+	return NOERR;
     }
     catch (std::exception const& e) {
 	printf("%s: exception '%s'\n", __func__, e.what());
@@ -249,8 +461,12 @@ STATUS v473_create_mooc_class(uint8_t cls)
 	printf("Error trying to add the reading handler.\n");
 	return ERROR;
     }
-    if (NOERR != add_class_msg(cls, rPRSET, (PMETHOD) devReading)) {
+    if (NOERR != add_class_msg(cls, rPRSET, (PMETHOD) devReadSetting)) {
 	printf("Error trying to add the reading-of-the-setting handler.\n");
+	return ERROR;
+    }
+    if (NOERR != add_class_msg(cls, sPRSET, (PMETHOD) devSetting)) {
+	printf("Error trying to add the setting handler.\n");
 	return ERROR;
     }
 
