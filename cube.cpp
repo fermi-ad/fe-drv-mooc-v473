cdf411a63a95d1c62e90a6288e7c53851a28462c
neswold <>
2011-01-12 22:05:29 +0000

    Disable the trigger while updating.

--- cube.cpp
+++ cube.cpp
@@ -231,23 +231,24 @@ STATUS v473_cube(V473::HANDLE const hw)
 
 	    {
 		vwpp::Lock lock(hw->mutex);
-
-		ssmBaseAddr->led[5] = Yellow;
-		hw->setRamp(lock, 0, ramp + 1, 0, data[0], 2 * (sizeof(path) / sizeof(*path) + 1));
-		ssmBaseAddr->led[5] = Black;
-		ssmBaseAddr->led[6] = Yellow;
-		hw->setRamp(lock, 1, ramp + 1, 0, data[1], 2 * (sizeof(path) / sizeof(*path) + 1));
-		ssmBaseAddr->led[6] = Black;
-
-		// Hand the $0f event to the interrupt assigned to the
-		// next ramp.
-
 		uint8_t const unevent = 0xfe;
-		uint8_t const event = 0x0f;
 
 		ssmBaseAddr->led[5] = Yellow;
 		hw->setTriggerMap(lock, !ramp, &unevent, 1);
 		ssmBaseAddr->led[5] = Black;
+
+		ssmBaseAddr->led[6] = Yellow;
+		hw->setRamp(lock, 0, ramp + 1, 0, data[0], 2 * (sizeof(path) / sizeof(*path) + 1));
+		ssmBaseAddr->led[6] = Black;
+		ssmBaseAddr->led[5] = Yellow;
+		hw->setRamp(lock, 1, ramp + 1, 0, data[1], 2 * (sizeof(path) / sizeof(*path) + 1));
+		ssmBaseAddr->led[5] = Black;
+
+		// Hand the $0f event to the interrupt assigned to the
+		// next ramp.
+
+		uint8_t const event = 0x0f;
+
 		ssmBaseAddr->led[6] = Yellow;
 		hw->setTriggerMap(lock, ramp, &event, 1);
 		ssmBaseAddr->led[6] = Black;
