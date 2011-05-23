1408f059078a2440442fb8f9f2dfe783da2bd1ca
mcarthur <>
2011-05-23 13:54:44 +0000

    First commit

---
+++ test_v473.cpp
@@ -0,0 +1,31 @@
+
+#include "v473.h"
+#include <cmath>
+#include <taskLib.h>
+
+
+STATUS v473_autotest(V473::HANDLE const hw)
+{
+    try {
+	{
+	    // Setup the hardware
+
+	    uint16_t data;
+	    vwpp::Lock lock(hw->mutex);
+
+	    hw->waveformEnable(lock, 0, false);
+	    hw->waveformEnable(lock, 1, false);
+	    
+	    printf("Hello, world\r\n");
+
+
+	}
+
+    }
+    catch (std::exception const& e) {
+	printf("caught: %s\n", e.what());
+	return ERROR;
+    }
+
+    return OK;
+}
