d1dc7d41100bd95c700d0244fe19348afefbdb11
neswold <>
2012-04-13 18:29:00 +0000

    Wait for interrupt doesn't work.

--- cube.cpp
+++ cube.cpp
@@ -248,8 +248,8 @@ STATUS v473_cube(V473::HANDLE const hw)
 	    // Wait for the ramp to start to play. Then we can switch
 	    // to updating the other ramp.
 
-	    while (hw->getActiveInterruptLevel(lock) != ramp)
-		taskDelay(1);
+	    // while (hw->getActiveInterruptLevel(lock) != ramp)
+	    taskDelay(4);
 	    ramp = !ramp;
 	} while (true);
     }
